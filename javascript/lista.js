f834d045b70fbeb405ca4466fd60eb4a18b016a1
Aquiles Aguiar <aquilesvibe@gmail.com>
2021-06-28 21:53:09 -0300

    Contem erro

--- javascript/lista.js
+++ javascript/lista.js
@@ -3,25 +3,26 @@ armazenamento.forEach(element => {
   var corpoTabela = document.getElementById('produtosLista')
     if(element.ativo === true) {
       let inputId = "inputCodigo" + element.cod
+      let trId = "trId" + element.cod
       let checkboxId = "checkboxCodigo" + element.cod
-      var linha = `<tr id = "linhaCompras">
+      var linha = `<tr id = "${trId}">
                       <td>${element.cod}</td>
                       <td>${element.nome}</td>
                       <td>${element.unid}</td>
                       <td>${element.quant}</td>
-                      <td><input type="number" id="${inputId}" onchange="atualizaValor(${element.cod}, ${element.quant})" value="${element.codBarras}"></td>
-                      <td><input type="checkbox" disabled id="${checkboxId}"></td>
+                      <td><input type="number" id="${inputId}" onfocusout="atualizaValor(${element.cod}, ${element.quant})" value="${element.codBarras}"></td>
+                      <td><input type="checkbox" disabled id="${checkboxId}" checked="${element.coletado}"></td>
                   </tr>`
       corpoTabela.innerHTML += linha
-      console.log(element.cod)
       RiscaLinha(element.cod)
     }
 })
 
 
 function RiscaLinha(cod){
-  if (armazenamento[cod - 1].coletado === true){
-    document.getElementById('linhaCompras').classList.add("risca")
+  if (armazenamento[cod-1].coletado === true){
+    console.log("trId" + cod)
+    document.getElementById("trId" + cod).classList.add("risca")
     document.getElementById('inputCodigo' + cod).setAttribute("disabled","disabled")
     document.getElementById("checkboxCodigo" + cod).checked = true
   } 
@@ -29,19 +30,17 @@ function RiscaLinha(cod){
 }
 
 function atualizaValor(cod, quantidade) {
-  let input
-  let checkbox
+  let input,checkbox
   input = document.getElementById('inputCodigo' + cod)
   checkbox = document.getElementById('checkboxCodigo' + cod)
   armazenamento[cod - 1].codBarras = Number(input.value)
   
 
   if(Number(input.value) >= Number(quantidade)) {
-    checkbox.checked = true
+    console.log(typeof(cod))
+    console.log(cod)
+    RiscaLinha(cod)
     armazenamento[cod - 1].coletado = true
-  }
-  else {
-    checkbox.checked = false
   }
   localStorage.setItem('listaCompras', JSON.stringify(armazenamento))
 }
